e56eaa407aa78a6545f702214233a6bc990ed0d3
Alex Bako <alex.bako@protonmail.com>
2023-01-02 21:26:13 +0100

    feat: add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -44,6 +44,13 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(message);
   }
